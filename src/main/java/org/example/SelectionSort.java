2f693e6c06e3c178588af66f8bdbab76745b6354
Geeth-Rath <geeth4862@gmail.com>
2024-01-11 23:01:56 +0530

    Selection sort completed

--- src/main/java/org/example/SelectionSort.java
+++ src/main/java/org/example/SelectionSort.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SelectionSort {
-}
+    public void getSort(){
+        ArrayList<Integer> numbList = new ArrayList<>(Arrays.asList(7, 4, 6, 9, 1));
+        System.out.println("Befor swap: "+ numbList);
+        for (int i = 0; i <= numbList.size()-1; i++) {
+            for (int j = i+1; j <= numbList.size()-1 ; j++) {
+                if (numbList.get(i) > numbList.get(j)) {
+                    swap(i , j, numbList);
+                }
+                }
+            }
+        System.out.println("After Swap: " + numbList);
+        }
+        public void swap(int i, int j, ArrayList list){
+            int temp = (int) list.get(i);
+            list.set(i , list.get(j));
+            list.set(j , temp);
+
+        }
+
+    }
+
